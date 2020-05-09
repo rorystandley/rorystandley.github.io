5927c499ee34a3a11fc6da7a4a83ec829c55a15f
Rory Standley <rorystandley@gmail.com>
2020-05-09 23:16:41 +0100

    Adding footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,7 @@ import { filter } from "rxjs/operators";
 	template: `
         <app-header></app-header>
         <router-outlet></router-outlet>
+		<app-footer></app-footer>
 	`
 } )
 export class AppComponent {
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 
 @NgModule( {
-	declarations: [ HeaderComponent ],
+	declarations: [
+		HeaderComponent,
+		FooterComponent
+	],
 	imports: [
 		CommonModule,
+		FontAwesomeModule
 	],
 	exports: [
-		HeaderComponent
+		HeaderComponent,
+		FooterComponent
 	]
 } )
 export class SharedModule {
